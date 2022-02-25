86000bfc7849c55c7b79f30741792a9d051496fb
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-25 18:54:20 +0100

    added initial server start and default middlewares

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
 const connectToDB = require("./database");
+const app = require("./server");
+const startServer = require("./server/startServer");
 
 const connectionString = process.env.CONN_STRING;
+const port = process.env.port || 4000;
 
 (async () => {
   await connectToDB(connectionString);
+  await startServer(port, app);
 })();
