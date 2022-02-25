0731f3d4a5549b57ad3d5860e6fe8e6d8ca8fca3
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-25 19:06:12 +0100

    added error handlers

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const { default: helmet } = require("helmet");
+const { notFoundError, errorHandler } = require("./middlewares/errorHandlers");
 
 const app = express();
 
@@ -9,5 +10,8 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(express.json());
+
+app.use(notFoundError);
+app.use(errorHandler);
 
 module.exports = app;
