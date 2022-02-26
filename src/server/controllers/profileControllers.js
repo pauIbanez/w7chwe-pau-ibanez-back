205cd4e183693356934de8943776679af8b775f0
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-26 14:50:36 +0100

    added getProfile controller

--- src/server/controllers/profileControllers.js
+++ src/server/controllers/profileControllers.js
@@ -2,18 +2,9 @@ const User = require("../../database/models/User");
 
 const listProfiles = async (req, res, next) => {
   try {
-    const profiles = await User.find();
-    const profilesToShare = profiles.map((profile) => {
-      const newProfile = {
-        name: profile.name,
-        lastName: profile.lastName,
-        username: profile.username,
-        friends: profile.friends.length,
-      };
-      return newProfile;
-    });
+    const profiles = await User.find().select("-password");
 
-    res.json({ profiles: profilesToShare });
+    res.json({ profiles });
   } catch (error) {
     const newError = { ...error };
     newError.message = "There was an error while getting the user profiles";
@@ -23,8 +14,18 @@ const listProfiles = async (req, res, next) => {
 
 const getProfile = async (req, res, next) => {
   const { id } = req.params;
+  try {
+    const profile = await User.findById(id)
+      .select("-password")
+      .populate("friends", "-password");
 
-  const friends = await User.findById(id).populate();
+    res.json(profile);
+  } catch (error) {
+    const newError = { ...error };
+    newError.message =
+      "There was an error while getting the user profile, please make sure you input a valid ID";
+    next(newError);
+  }
 };
 
 const updateProfile = (req, res, next) => {};
